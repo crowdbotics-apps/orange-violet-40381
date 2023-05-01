acac05eec1a37ab63a9960217c4f1297d0f02d06
Crowdbotics <team@crowdbotics.com>
2023-05-01 21:28:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orange-violet-40381",
+  url: "https://orange-violet-40381.botics.co",
+  api: "https://orange-violet-40381.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
